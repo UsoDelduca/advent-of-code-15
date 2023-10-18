63b1630d609cafa9563a817817e25eb0d2c53705
Rodolfo Delduca <usodelduca@gmail.com>
2023-10-18 16:07:37 +1300

    begin part 2

--- game-day-2.ts
+++ game-day-2.ts
@@ -2,13 +2,15 @@ import fs from 'fs'
 
 const input = fs.readFileSync('day2.txt').toString().split('\n')
 
+// PART 1 //
+
 function calculate(length: number, width: number, height: number) {
   const lw = length * width
   const wh = width * height
   const hl = height * length
   let smallest: number = Math.min(lw, wh, hl)
 
-  console.log('lwh', lw, wh, hl)
+  // console.log('lwh', lw, wh, hl)
 
   return 2 * lw + 2 * wh + 2 * hl + smallest
 }
@@ -20,7 +22,16 @@ input.map((dimension) => {
   // console.log([length, width, height])
   dimensions.push(calculate(Number(length), Number(width), Number(height)))
 })
-console.log('DIMENSIONS 2: ', dimensions[2])
+// console.log('DIMENSIONS 2: ', dimensions[2])
 const arrSum = (arr: number[]) => arr.reduce((sum, curr) => sum + curr, 0)
 
 console.log(arrSum(dimensions))
+
+// PART 2 //
+const perimeterArr: any[] = []
+input.map((perimeter) => {
+  const [length, width, height] = perimeter.split('x')
+  perimeterArr.push([Number(length), Number(width), Number(height)])
+})
+
+console.log(perimeterArr)
