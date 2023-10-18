b80d5e48ff9ba2d6e4c071977b794031e8072e72
Rodolfo Delduca <usodelduca@gmail.com>
2023-10-18 13:42:23 +1300

    add day 2 file

---
+++ game-day-2.ts
@@ -0,0 +1,17 @@
+import fs from 'fs'
+
+const input = fs.readFileSync('day2.txt').toString().split('\n')
+
+const dimensions: number[] = []
+input.map((dimension) => {
+  const [length, width, height] = dimension.split('x')
+  // console.log([length, width, height])
+  dimensions.push(
+    calculate(parseInt(length), parseInt(width), parseInt(height))
+  )
+})
+console.log(dimensions)
+
+function calculate(length: number, width: number, height: number) {
+  return 2 * (length * width) + 2 * (width * height) + 2 * (height * length)
+}
